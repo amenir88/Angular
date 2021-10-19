3df119a28aca3e11356d0d60ab2f13fe8e5c1848
amenir88 <ameni.rommene@esprit.tn>
2021-10-19 11:24:52 +0100

    reactive form

--- app4TWIN1/src/app/app.module.ts
+++ app4TWIN1/src/app/app.module.ts
@@ -5,13 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListuserComponent } from './listuser/listuser.component';
 import { HeaderComponent } from './header/header.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MainUserComponent } from './main-user/main-user.component';
 import { MainProductComponent } from './main-product/main-product.component';
 import { MainProviderComponent } from './main-provider/main-provider.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 import { HomeComponent } from './home/home.component';
 import { FormUserComponent } from './form-user/form-user.component';
+import { LoginUserComponent } from './login-user/login-user.component';
 
 @NgModule({
   declarations: [
@@ -23,12 +24,15 @@ import { FormUserComponent } from './form-user/form-user.component';
     MainProviderComponent,
     NotFoundPageComponent,
     HomeComponent,
-    FormUserComponent
+    FormUserComponent,
+    LoginUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule, //template driven : ngModel et ngForm
+    ReactiveFormsModule //formGroup, formControlName
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- app4TWIN1/src/app/form-user/form-user.component.ts
+++ app4TWIN1/src/app/form-user/form-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-form-user',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-user.component.css']
 })
 export class FormUserComponent implements OnInit {
-
+  categorie="Customer";
+  list : User[] = [];
+  user : User = new User();
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addUser(){
+    this.user.idCustomer=this.list.length + 1;
+    this.user.accountCategory=this.categorie;
+    this.list.push(this.user);
+    this.user=new User();
+   console.log(this.list);
   }
 
 }
